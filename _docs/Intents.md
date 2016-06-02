98f905ba324d49ff75137d13c4cd9e0920db4eb1
joe <joe@sonrisa.hu>
2016-06-02 23:25:52 +0200

    WIP - LUIS model: login (user, pass), logout

---
+++ _docs/Intents.md
@@ -0,0 +1,8 @@
+## The following intents should be recognized by Sontime Chat Bot:
+
+- Login with a **[login name]** and a **[password]**
+- Logout
+- List my activities **[on a given day:today]**.
+- Get a sum of my activities **[on a given day/current week/last week:today]**
+- Record a new activity for a **[given ticket]** with a **[given time interval]** on today.
+- ???
--- bot.js
+++ bot.js
@@ -2,53 +2,129 @@ var restify = require('restify');
 var builder = require('botbuilder');
 var util = require('util');
 
-
-module.exports  = {
-
-  main: function () {
-    // Create bot and add dialogs
-    var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
-
-    // Create LUIS Dialog that points at our model and add it as the root '/' dialog for our Cortana Bot.
-    var model = 'https://api.projectoxford.ai/luis/v1/application?id=edfc7f9a-c512-40b7-a9ec-215a6c75b199&subscription-key=c35fcb1a65064435a3b6d8ae1131f609&q=';
-    var dialog = new builder.LuisDialog(model);
-    bot.add('/', dialog);
+// URL of LUIS model published as a HTTP service
+var LUIS_URL = "https://api.projectoxford.ai/luis/v1/application?id=1bb1085a-f7b1-4f91-9e37-e9a8f10dd7e3&subscription-key=c35fcb1a65064435a3b6d8ae1131f609";
 
 
-    // Add intent handlers
-    dialog.on('OrganizeMeeting',
-      function (session, results) {
-        console.log('result: %s', util.inspect(results, false, null));
-        session.send('Set a meeting at ..... ');
-    });
+module.exports = {
 
-    dialog.on('CancelMeeting',
-      function (session, results) {
-        console.log('result: %s', util.inspect(results, false, null));
-        session.send('Cancel your meeting at ..... ');
-    });
+    main: function () {
+        // Create bot and add dialogs
+        var bot = new builder.BotConnectorBot({appId: 'YourAppId', appSecret: 'YourAppSecret'});
 
-    // dialog.on('builtin.intent.alarm.delete_alarm', builder.DialogAction.send('Deleting Alarm'));
-    dialog.onDefault(
+        var dialog = new builder.LuisDialog(LUIS_URL);
+        bot.add('/', dialog);
+        bot.add('/promptUserName', promptUserNameDialog);
+        // bot.add('/promptPassword', );
 
-      function (session, results) {
-          console.log('result: %s', util.inspect(results, false, null));
-          session.send("I don't understand.");
+        // Add intent handlers
+        dialog.on('Login', onLoginIntent);
+        dialog.on('Logout', onLogoutIntent);
+        dialog.onDefault(onDefault);
+
+
+        // Setup Restify Server
+        var server = restify.createServer();
+        server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+        server.listen(process.env.port || 3978, function () {
+            console.log('%s listening to %s', server.name, server.url);
+        });
+
+    }
+
+}
+
+
+var onLoginIntent = function (session, results) {
+    console.log('result: %s', util.inspect(results, false, null));
+
+
+    var loginName = builder.EntityRecognizer.findEntity(results.entities, 'username');
+    if (loginName){
+        session.userData.login = loginName.entity;
+    }
+
+    if (noUserNameInSession(session) || noPasswordInSession(session)) {
+        session.send("I think you'd like to login to Sontime, but I don't know your full credentials.");
+        session.beginDialog('/promptUserName');
+    }else{
+        session.send("I think you'd like to login to Sontime, and I know your user name (%s.) and your password too.", session.userData.login);
+    }
+}
+
+
+var onLogoutIntent = function (session, results) {
+    console.log('result: %s', util.inspect(results, false, null));
+    session.send("So you'd like to logout from Sontime.");
+    session.userData.login = "";
+    session.userData.password = "";
+}
+
+
+var onDefault = function (session, results) {
+    console.log('result: %s', util.inspect(results, false, null));
+    session.send("Sorry but I don't understand. Doh! I'm just a bot actually. :)");
+}
+
+// asks for login name
+var promptUserNameDialog = [
+        function (session, next) {
+            if (noUserNameInSession(session)) {
+                builder.Prompts.text(session, "What's you login name?");
+            }else{
+                // console.log('login: %s', util.inspect(session.userData.login, false, null));
+                session.send("I already know your name: %s", session.userData.login);
+                next();
+
+                // FIXME: if you give your login name as "login as joe", the next() doesn't jump to the next function
+            }
+        },
+        function (session, results) {
+            if (results && results.response){
+                session.userData.login = results.response;
+               next();
+            }else{
+               next();
+            }
+        },
+        function (session, next){
+            if (noPasswordInSession(session)) {
+                builder.Prompts.text(session, "What's you password?");
+            }else{
+                session.send("I already know your password");
+                next();
+            }
+        },
+        function (session, results, next) {
+        if (results.response){
+            session.userData.password= results.response;
+            session.endDialog();
         }
-      //builder.DialogAction.send("I'm sorry I didn't understand. I can only organize meetings.")
+    }
+    ];
 
-    );
+// asks for password
+var promptPasswordDialog = [
+        function (session, next) {
+            if (noPasswordInSession(session)) {
+                builder.Prompts.text(session, "What's you password?");
+            }else{
+                session.send("I already know password.");
+                session.endDialog();
+            }
+        },
+        function (session, results) {
+            session.userData.password = results.response;
+            session.endDialog();
+        }
+    ];
 
+// returns true, if there is no user name in session's userData object.
+var noUserNameInSession = function (session){
+    return !session.userData.login;
+}
 
-
-
-    // Setup Restify Server
-    var server = restify.createServer();
-    server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-    server.listen(process.env.port || 3978, function () {
-        console.log('%s listening to %s', server.name, server.url);
-    });
-
-   },
-
+// returns true, if there is no password in session's userData object.
+var noPasswordInSession = function (session){
+    return !session.userData.password;
 }

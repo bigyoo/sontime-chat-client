791a9bd03bad3639341376bcff0ddcffc4881629
joe <joe@sonrisa.hu>
2016-06-17 23:47:51 +0200

    WIP: Bot refactor -- logout intent stucks into infinit loop -- bot tests need to be  synchronized to new replies

--- app/bot/bot.js
+++ app/bot/bot.js
@@ -5,28 +5,21 @@ var builder = require('botbuilder');
 var restify = require('restify');
 var prompts = require('./prompts');
 
-// TODO sontime need to be mocked in bot tests
-var sontime = require('../sontime');
 
-// Create bot
 var sontimeChatBot = {
     botInstance: new builder.BotConnectorBot({appId: 'YourAppId', appSecret: 'YourAppSecret'}),
     LUIS_URL: "https://api.projectoxford.ai/luis/v1/application?id=1bb1085a-f7b1-4f91-9e37-e9a8f10dd7e3&subscription-key=c35fcb1a65064435a3b6d8ae1131f609"
 
 };
 
-sontimeChatBot.setupBot = function(){
-    var dialog = new builder.LuisDialog(sontimeChatBot.LUIS_URL);
+var userContext = {
+    login: null,
+    password: null
+}
 
-    sontimeChatBot.botInstance.add('/', dialog);
-    sontimeChatBot.botInstance.add('/promptUserName', promptUserNameDialog);
-    sontimeChatBot.botInstance.add('/promptPassword', promptPasswordDialog);
-
-    // Add intent handlers
-    dialog.on('Login', onLoginIntent);
-    dialog.on('Logout', onLogoutIntent);
-    dialog.onDefault(onDefault);
-};
+// --------------------------------------------------------------------
+// ~ Bot configuration
+// --------------------------------------------------------------------
 
 sontimeChatBot.startBotServer = function(){
         // Setup Restify Server
@@ -37,44 +30,49 @@ sontimeChatBot.startBotServer = function(){
         });
 };
 
+sontimeChatBot.setupBot = function(){
+    var dialog = new builder.LuisDialog(sontimeChatBot.LUIS_URL);
+
+    sontimeChatBot.botInstance.add('/', dialog);
+    sontimeChatBot.botInstance.add('/loginDialog', loginDialogChain);
+    sontimeChatBot.botInstance.add('/logoutDialog', logoutDialogChain);
+
+    // Add intent handlers
+    dialog.on('Login', onLoginIntent);
+    dialog.on('Logout', onLogoutIntent);
+    dialog.onDefault(onDefault);
+};
+
+// --------------------------------------------------------------------
+// ~ Intent handlers
+// --------------------------------------------------------------------
+
 var onLoginIntent = function (session, results) {
     console.log('Login intent: %s', util.inspect(results, false, null));
 
-
+    // if the intent contains a login name, it will be put into the user context
     var loginName = builder.EntityRecognizer.findEntity(results.entities, 'username');
     if (loginName){
-        session.userData.login = loginName.entity;
+        userContext.login = loginName.entity;
     }
-    
+
+    // if the intent contains a password, it will be put into the user context
     var password = builder.EntityRecognizer.findEntity(results.entities, 'password');
     if (password){
-        session.userData.password = password.entity;
+        userContext.password = password.entity;
     }
 
-    if (noUserNameInSession(session) || noPasswordInSession(session)) {
-        session.send(prompts.login_intent_without_login_or_password);
-        session.beginDialog('/promptUserName');
-    }else{
-        session.send(prompts.login_intent_with_login_and_password, session.userData.login);
-
-        // TODO: here i'm just playing with sontime API
-        sontime.getUser(session.userData.login, session.userData.password)
-            .then(function (sontimeUser) {
-                session.send(util.format(prompts.login_success, session.userData.login));
-                return sontimeUser.getResource('v2/projects/100');
-            }).then(function (project) {
-                console.log(project);
-                session.send(util.format('Your project is %s, the customer is %s ',project.name, project.company.name) );
-            });
-    }
+    // starts a new login dialog
+    session.beginDialog('/loginDialog');
 }
 
 
 var onLogoutIntent = function (session, results) {
     console.log('result: %s', util.inspect(results, false, null));
-    session.send(prompts.logout_intent);
-    session.userData.login = "";
-    session.userData.password = "";
+
+    // FIXME: this dialog stucks into an infinit loop
+    session.beginDialog('/logoutDialog');
+
 }
 
 
@@ -83,66 +81,97 @@ var onDefault = function (session, results) {
     session.send(prompts.default_intent);
 }
 
-/**
- * Asks for login name.
- */
-var  promptUserNameDialog = [
-        function (session, results, next) {
-            if (noUserNameInSession(session)) {
-                builder.Prompts.text(session, prompts.prompt_login_name);
-            }else if(noPasswordInSession(session)){
-                session.send(prompts.login_intent_with_login_but_without_password, session.userData.login);
-                session.beginDialog("/promptPassword");
-            }else{
-                // we've got the username and password in session
-                session.endDialog();
-            }
-        },
-        function (session, results) {
-            if (results && results.response){
-                session.userData.login = results.response;
-            }
+// --------------------------------------------------------------------
+// ~ Dialog chains
+// --------------------------------------------------------------------
 
-            if(noPasswordInSession(session)){
-                session.beginDialog("/promptPassword");
-            }else{
-                session.endDialog();
-            }
-        }];
+var logoutDialogChain = [
+    function (session, results, next){
+        session.send(prompts.logout_intent);
+        next();
+    },
+    function(session, results, next){
+        userContext.login = null;
+        userContext.password = null;
+        next();
+    },
+    function (session, results, next){
+        session.endDialog(results);
+    },
+]
 
-
-var promptPasswordDialog = [
-        function (session, next){
-            if (noPasswordInSession(session)) {
-                builder.Prompts.text(session, prompts.prompt_password);
-            }else{
-                session.send(prompts.already_know_you_password);
-                session.endDialog();
-            }
-        },
-        function (session, results, next) {
-            if (results.response){
-                session.userData.password= results.response;
-            }
-            session.endDialog();
+var loginDialogChain = [
+    function (session, results, next) {
+        // prompts for login name if there is
+        if (noUserNameInContext()) {
+            builder.Prompts.text(session, prompts.prompt_login_name);
+        }else{
+            next();
+        }
+    },
+    function (session, results, next) {
+        // stores the login name
+        if (results && results.response){
+            userContext.login = results.response;
+        }
+        next();
+    },
+    function (session, results, next){
+        if (noPasswordInContext()) {
+            builder.Prompts.text(session, prompts.prompt_password);
+        }else{
+            next();
+        }
+    },
+    function (session, results, next) {
+        if (results.response){
+            userContext.password= results.response;
+        }
+        next();
+    },
+    function (session) {
+        session.send(prompts.login_intent_with_login_and_password + " %s", userContext.login, userContext.password);
+        session.endDialog();
     }
-    ];
-
-
-/**
- * Returns true, if there is no user name in session's userData object.
- *
- */
-var noUserNameInSession = function (session){
-    return !session.userData.login;
-}
-
-/**
- * Returns true, if there is no password in session's userData object.
- */
-var noPasswordInSession = function (session){
-    return !session.userData.password;
-}
+];
 
 
 module.exports = sontimeChatBot;
+
+// -------------------------------------------------------------------------
+// ~ Private functions
+// -------------------------------------------------------------------------
+
+/**
+ * Returns true, if there is no user name in the user context object.
+ *
+ */
+var noUserNameInContext = function (){
+    return !userContext.login;
+}
+
+/**
+ * Returns true, if there is no password in the user context object.
+ */
+var noPasswordInContext = function (){
+    return !userContext.password;
+}
+
+// // TODO sontime need to be mocked in bot tests
+// var sontime = require('../sontime');
+
+function playingWithSontime() {
+
+    //
+    //     // // TODO: here i'm just playing with sontime API
+    //     // sontime.getUser(userContext.login, userContext.password)
+    //     //     .then(function (sontimeUser) {
+    //     //         session.send(util.format(prompts.login_success, userContext.login));
+    //     //         return sontimeUser.getResource('v2/projects/100');
+    //     //     }).then(function (project) {
+    //     //         console.log(project);
+    //     //         session.send(util.format('Your project is %s, the customer is %s ',project.name, project.company.name) );
+    //     //     });
+    // }
+
+}
